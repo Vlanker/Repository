ce773215b9db9fec4d339cba1c7dd0be0e197c74
Vlanker <kinvlanknik@yandex.ru>
2016-03-24 23:49:49 +0300

    счётчик

---
+++ MyUtilits/MyUtilits/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MyUtilits
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.tc = new System.Windows.Forms.TabControl();
+            this.tp1 = new System.Windows.Forms.TabPage();
+            this.tp2 = new System.Windows.Forms.TabPage();
+            this.btnPl = new System.Windows.Forms.Button();
+            this.btnRes = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.lbCount = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tc.SuspendLayout();
+            this.tp1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile,
+            this.tsmiAbout});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiExit});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(48, 20);
+            this.tsmiFile.Text = "&Файл";
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(152, 22);
+            this.tsmiExit.Text = "&Выход";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // tsmiAbout
+            // 
+            this.tsmiAbout.Name = "tsmiAbout";
+            this.tsmiAbout.Size = new System.Drawing.Size(94, 20);
+            this.tsmiAbout.Text = "&О программе";
+            this.tsmiAbout.Click += new System.EventHandler(this.tsmiAbout_Click);
+            // 
+            // tc
+            // 
+            this.tc.Controls.Add(this.tp1);
+            this.tc.Controls.Add(this.tp2);
+            this.tc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tc.Location = new System.Drawing.Point(0, 24);
+            this.tc.Name = "tc";
+            this.tc.SelectedIndex = 0;
+            this.tc.Size = new System.Drawing.Size(284, 237);
+            this.tc.TabIndex = 1;
+            // 
+            // tp1
+            // 
+            this.tp1.Controls.Add(this.lbCount);
+            this.tp1.Controls.Add(this.btnMin);
+            this.tp1.Controls.Add(this.btnRes);
+            this.tp1.Controls.Add(this.btnPl);
+            this.tp1.Location = new System.Drawing.Point(4, 22);
+            this.tp1.Name = "tp1";
+            this.tp1.Padding = new System.Windows.Forms.Padding(3);
+            this.tp1.Size = new System.Drawing.Size(276, 211);
+            this.tp1.TabIndex = 0;
+            this.tp1.Text = "Счётчик";
+            this.tp1.UseVisualStyleBackColor = true;
+            // 
+            // tp2
+            // 
+            this.tp2.Location = new System.Drawing.Point(4, 22);
+            this.tp2.Name = "tp2";
+            this.tp2.Padding = new System.Windows.Forms.Padding(3);
+            this.tp2.Size = new System.Drawing.Size(276, 211);
+            this.tp2.TabIndex = 1;
+            this.tp2.Text = "tabPage2";
+            this.tp2.UseVisualStyleBackColor = true;
+            // 
+            // btnPl
+            // 
+            this.btnPl.Location = new System.Drawing.Point(6, 6);
+            this.btnPl.Name = "btnPl";
+            this.btnPl.Size = new System.Drawing.Size(37, 23);
+            this.btnPl.TabIndex = 0;
+            this.btnPl.Text = "+";
+            this.btnPl.UseVisualStyleBackColor = true;
+            this.btnPl.Click += new System.EventHandler(this.btnPl_Click);
+            // 
+            // btnRes
+            // 
+            this.btnRes.Location = new System.Drawing.Point(49, 6);
+            this.btnRes.Name = "btnRes";
+            this.btnRes.Size = new System.Drawing.Size(75, 23);
+            this.btnRes.TabIndex = 1;
+            this.btnRes.Text = "Сброс";
+            this.btnRes.UseVisualStyleBackColor = true;
+            this.btnRes.Click += new System.EventHandler(this.btnRes_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(6, 48);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(37, 23);
+            this.btnMin.TabIndex = 2;
+            this.btnMin.Text = "-";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // lbCount
+            // 
+            this.lbCount.AutoSize = true;
+            this.lbCount.Location = new System.Drawing.Point(8, 32);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(0, 13);
+            this.lbCount.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tc);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Мои утилиты";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tc.ResumeLayout(false);
+            this.tp1.ResumeLayout(false);
+            this.tp1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAbout;
+        private System.Windows.Forms.TabControl tc;
+        private System.Windows.Forms.TabPage tp1;
+        private System.Windows.Forms.Label lbCount;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnRes;
+        private System.Windows.Forms.Button btnPl;
+        private System.Windows.Forms.TabPage tp2;
+    }
+}
+
---
+++ MyUtilits/MyUtilits/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyUtilits
+{
+    public partial class MainForm : Form
+    {
+        int count = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tsmiAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа\n\"мои утилиты\". \nАвтор: К ", "О программе");
+        }
+
+        private void btnPl_Click(object sender, EventArgs e)
+        {
+            count++;
+            lbCount.Text = count.ToString();
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+
+            count--;
+            lbCount.Text = Convert.ToString(count);
+        }
+
+        private void btnRes_Click(object sender, EventArgs e)
+        {
+
+            count = 0;
+            lbCount.Text = " ";
+        }
+    }
+}
